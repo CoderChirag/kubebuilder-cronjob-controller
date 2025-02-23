6ceed72842da7566c9d759d5dc8ebff7c3148e78
coderchirag <jain.chirag0174@gmail.com>
2025-02-23 22:17:43 +0530

    add admission mutating webhook & admission validation webhook

--- internal/webhook/v1/cronjob_webhook.go
+++ internal/webhook/v1/cronjob_webhook.go
@@ -20,6 +20,12 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/robfig/cron"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	validationutils "k8s.io/apimachinery/pkg/util/validation"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -37,11 +43,15 @@ var cronjoblog = logf.Log.WithName("cronjob-resource")
 func SetupCronJobWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).For(&batchv1.CronJob{}).
 		WithValidator(&CronJobCustomValidator{}).
-		WithDefaulter(&CronJobCustomDefaulter{}).
+		WithDefaulter(&CronJobCustomDefaulter{
+			DefaultConcurrencyPolicy: batchv1.AllowConcurrent,
+			DefaultSuspend: false,
+			DefaultSuccessfulJobHistoryLimit: 3,
+			DefaultFailedJobHistoryLimit: 1,
+		}).
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 // +kubebuilder:webhook:path=/mutate-batch-coderchirag-github-io-v1-cronjob,mutating=true,failurePolicy=fail,sideEffects=None,groups=batch.coderchirag.github.io,resources=cronjobs,verbs=create;update,versions=v1,name=mcronjob-v1.kb.io,admissionReviewVersions=v1
 
@@ -51,7 +61,10 @@ func SetupCronJobWebhookWithManager(mgr ctrl.Manager) error {
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as it is used only for temporary operations and does not need to be deeply copied.
 type CronJobCustomDefaulter struct {
-	// TODO(user): Add more fields as needed for defaulting
+	DefaultConcurrencyPolicy batchv1.ConcurrencyPolicy
+	DefaultSuspend bool
+	DefaultSuccessfulJobHistoryLimit int32
+	DefaultFailedJobHistoryLimit int32
 }
 
 var _ webhook.CustomDefaulter = &CronJobCustomDefaulter{}
@@ -65,12 +78,32 @@ func (d *CronJobCustomDefaulter) Default(ctx context.Context, obj runtime.Object
 	}
 	cronjoblog.Info("Defaulting for CronJob", "name", cronjob.GetName())
 
-	// TODO(user): fill in your defaulting logic.
+	d.applyDefaults(cronjob)
 
 	return nil
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+func (d *CronJobCustomDefaulter) applyDefaults(cronJob *batchv1.CronJob) {
+	if cronJob.Spec.ConcurrencyPolicy == "" {
+		cronJob.Spec.ConcurrencyPolicy = d.DefaultConcurrencyPolicy
+	}
+
+	if cronJob.Spec.Suspend == nil {
+		cronJob.Spec.Suspend = new(bool)
+		*cronJob.Spec.Suspend = d.DefaultSuspend
+	}
+
+	if cronJob.Spec.FailedJobHistoryLimit == nil {
+		cronJob.Spec.FailedJobHistoryLimit = new(int32)
+		*cronJob.Spec.FailedJobHistoryLimit = d.DefaultFailedJobHistoryLimit
+	}
+
+	if cronJob.Spec.SuccessfulJobHistoryLimit == nil {
+		cronJob.Spec.SuccessfulJobHistoryLimit = new(int32)
+		*cronJob.Spec.SuccessfulJobHistoryLimit = d.DefaultSuccessfulJobHistoryLimit
+	}
+}
+
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
 // +kubebuilder:webhook:path=/validate-batch-coderchirag-github-io-v1-cronjob,mutating=false,failurePolicy=fail,sideEffects=None,groups=batch.coderchirag.github.io,resources=cronjobs,verbs=create;update,versions=v1,name=vcronjob-v1.kb.io,admissionReviewVersions=v1
@@ -81,7 +114,6 @@ func (d *CronJobCustomDefaulter) Default(ctx context.Context, obj runtime.Object
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as this struct is used only for temporary operations and does not need to be deeply copied.
 type CronJobCustomValidator struct {
-	// TODO(user): Add more fields as needed for validation
 }
 
 var _ webhook.CustomValidator = &CronJobCustomValidator{}
@@ -94,9 +126,7 @@ func (v *CronJobCustomValidator) ValidateCreate(ctx context.Context, obj runtime
 	}
 	cronjoblog.Info("Validation for CronJob upon creation", "name", cronjob.GetName())
 
-	// TODO(user): fill in your validation logic upon object creation.
-
-	return nil, nil
+	return nil, validateCronJob(cronjob)
 }
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type CronJob.
@@ -107,9 +137,7 @@ func (v *CronJobCustomValidator) ValidateUpdate(ctx context.Context, oldObj, new
 	}
 	cronjoblog.Info("Validation for CronJob upon update", "name", cronjob.GetName())
 
-	// TODO(user): fill in your validation logic upon object update.
-
-	return nil, nil
+	return nil, validateCronJob(cronjob)
 }
 
 // ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type CronJob.
@@ -120,7 +148,49 @@ func (v *CronJobCustomValidator) ValidateDelete(ctx context.Context, obj runtime
 	}
 	cronjoblog.Info("Validation for CronJob upon deletion", "name", cronjob.GetName())
 
-	// TODO(user): fill in your validation logic upon object deletion.
 
 	return nil, nil
+}
+
+func validateCronJob(cronJob *batchv1.CronJob) error {
+	var allErrs field.ErrorList
+	if err := validateCronJobName(cronJob); err != nil {
+		allErrs = append(allErrs, err)
+	}
+
+	if err := validateCronJobSpec(cronJob); err != nil {
+		allErrs = append(allErrs, err)
+	}
+
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{Group: "batch.coderchirag.github.io", Kind: "CronJob"}, cronJob.Name, allErrs)
+}
+
+func validateCronJobSpec(cronJob *batchv1.CronJob) *field.Error {
+	// The field helpers from the kubernetes API machinery help us return nicely
+	// structured validation errors.
+	return validateScheduleFormat(cronJob.Spec.Schedule, field.NewPath("spec").Child("schedule"))
+}
+
+func validateScheduleFormat(schedule string, fldPath *field.Path) *field.Error {
+	if _, err := cron.Parse(schedule); err != nil {
+		return field.Invalid(fldPath, schedule, err.Error())
+	}
+	return nil
+}
+
+func validateCronJobName(cronJob *batchv1.CronJob) *field.Error {
+	if len(cronJob.ObjectMeta.Name) > validationutils.DNS1035LabelMaxLength-11 {
+		// The job name length is 63 characters like all Kubernetes objects
+		// (which must fit in a DNS subdomain). The cronjob controller appends
+		// a 11-character suffix to the cronjob (`-$TIMESTAMP`) when creating
+		// a job. The job name length limit is 63 characters. Therefore cronjob
+		// names must have length <= 63-11=52. If we don't validate this here,
+		// then job creation will fail later.
+		return field.Invalid(field.NewPath("metadata").Child("name"), cronJob.ObjectMeta.Name, "must be no more than 52 characters")
+	}
+	return nil
 }
