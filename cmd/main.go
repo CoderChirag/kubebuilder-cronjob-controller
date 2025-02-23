9f0879f2477bb16f483e273a380a10d8ba91a201
coderchirag <jain.chirag0174@gmail.com>
2025-02-23 21:36:27 +0530

    run kubebuilder create webhook --group batch --version v1 --kind CronJob --defaulting --programmatic-validation

--- cmd/main.go
+++ cmd/main.go
@@ -39,6 +39,7 @@ import (
 
 	batchv1 "coderchirag.github.io/cronjob/api/v1"
 	"coderchirag.github.io/cronjob/internal/controller"
+	webhookbatchv1 "coderchirag.github.io/cronjob/internal/webhook/v1"
 	// +kubebuilder:scaffold:imports
 )
 
@@ -208,6 +209,13 @@ func main() {
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "CronJob")
 		os.Exit(1)
+	}
+	// nolint:goconst
+	if os.Getenv("ENABLE_WEBHOOKS") != "false" {
+		if err = webhookbatchv1.SetupCronJobWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "CronJob")
+			os.Exit(1)
+		}
 	}
 	// +kubebuilder:scaffold:builder
 
